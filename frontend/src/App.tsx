d16cc51f707460619515fda4b5bc6889c64275e9
Rittiphon Phoarun <miw1513@hotmail.com>
2025-09-07 21:18:03 +0700

    first  commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import Users from './components/Users';
-import Header from './components/Header';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Header />
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
